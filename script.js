bc14a7c62344cae0033ee88f373740601eb2ad23
D-VARDHAN <128993267+D-VARDHAN@users.noreply.github.com>
2026-06-29 18:15:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('resumeBtn').addEventListener('click',()=>console.log('Resume clicked'));
+document.getElementById('githubLink').addEventListener('click',()=>console.log('GitHub clicked'));
+document.getElementById('linkedinLink').addEventListener('click',()=>console.log('LinkedIn clicked'));
